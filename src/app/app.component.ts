1a45c205877c40d5aba99a27804dd1be1f0518fb
Anmol Narang <anmolnarang49@gmail.com>
2019-10-09 19:22:53 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Make-Me-Sustainable';
+  flipDiv: boolean = false;
+  flipCard() {
+  console.log('flip'+this.flipDiv);
+  if(this.flipDiv){
+   this.flipDiv = false;
+   
+  }
+   else
+    {
+    this.flipDiv = true;
+    }   
+  }
+   cardData=
+[
+   {"img":"", "description":"1"  },
+  { "img":"", "description":"2" },
+  { "img":"", "description":"3"  }
+ 
+]
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//import { SideBarModule } from 'ng-sidebar' ;
+import { FlipModule } from 'ngx-flip';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule ,
+    FlipModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+private _opened: boolean = false;
+flipDiv: boolean = false;
+  private _toggleSidebar() {
+    this._opened = !this._opened;
+  }
+
+
+}
+/*
+flipCard() {
+   console.log('flip'+this.flipDiv);
+   if(this.flipDiv){
+    this.flipDiv = false;
+    
+   }
+    else
+{
+  this.flipDiv = true;
+}   */
